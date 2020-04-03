4255e77d3f78751711da450bf4e50805bc3a6936
prachi96 <arorasm13@gmail.com>
2020-04-04 00:16:59 +0530

    PA: Added routing for menu items

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
 import React from "react";
+import { Switch, Route } from "react-router-dom";
 import "./App.css";
 import HomePage from "./components/templates/HomePage";
+
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE </h1>
+  </div>
+);
 
 function App() {
   return (
     <div className="App">
-      <HomePage />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/hats" component={HatsPage} />
+      </Switch>
     </div>
   );
 }
+
+/**
+ * In case of Switch, if it gets a first match,
+ * it renders that component and doesn't traverse other routes.
+ * So we don't accidently render multiple components.
+ */
 
 export default App;
