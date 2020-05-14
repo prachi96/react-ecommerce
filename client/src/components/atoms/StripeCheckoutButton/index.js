375f270650e919953647e281950e5a39a8a4e4fc
prachi96 <arorasm13@gmail.com>
2020-05-14 20:41:42 +0530

    PA : Added server config for stripe and changed folder structure

--- client/src/components/atoms/StripeCheckoutButton/index.js
+++ client/src/components/atoms/StripeCheckoutButton/index.js
@@ -1,12 +1,29 @@
 import React from "react";
 import StripeCheckout from "react-stripe-checkout";
+import axios from "axios";
 
 const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100;
   const publishableKey = "pk_test_pMCpNnlO62lF0CGPvjVQPrdR0067B4PWtX";
 
   const onToken = (token) => {
-    console.log("Payment successful : ", token);
+    axios({
+      url: "payment",
+      method: "post",
+      data: {
+        amount: priceForStripe,
+        source: token.id,
+      },
+    })
+      .then((response) => {
+        alert("succesful payment");
+      })
+      .catch((error) => {
+        console.log("Payment Error: ", error);
+        alert(
+          "There was an issue with your payment! Please make sure you use the provided credit card."
+        );
+      });
   };
 
   return (
